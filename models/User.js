56e1ee81ece4a3ba28da3603f4f7bdeed975d805
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-03-16 12:12:57 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,9 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+// create our User model
 class User extends Model {
+   // set up method to run on instance data (per user) to check password
   checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
@@ -31,6 +33,7 @@ User.init(
   },
   {
     hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
       beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
